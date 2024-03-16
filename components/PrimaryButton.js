0b5c637fa4616f32b6e6f742534d6c8ec0735895
Muhammad Zain <muhammadzain0742@gmail.com>
2024-03-16 14:20:52 +0500

    Initial Commit

---
+++ components/PrimaryButton.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+
+const PrimaryButton = (props) => {
+
+  const pressHandler = () => {
+    console.log("Pressed Primary BUtton");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] :styles.buttonInnerContainer} onPress={pressHandler} android_ripple={{ color: "#640233" }}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default PrimaryButton
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+    // width: '40%'
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  pressed: {
+    opacity: 0.75
+  }
+})
---
+++ screens/GameOverScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {TextInput, View} from 'react-native'
+
+const GameOverScreen = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default GameOverScreen
